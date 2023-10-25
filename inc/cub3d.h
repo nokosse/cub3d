8dc997cf7c5e99e23b2b20ab460034d4c6938044
nokosse <visouthivong.kevin@gmail.com>
2023-10-25 13:35:35 +0200

    ajout includes

---
+++ inc/cub3d.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 13:21:53 by kvisouth          #+#    #+#             */
+/*   Updated: 2023/10/25 13:30:00 by kvisouth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "../libft/inc/libft.h"
+# include "../libft/inc/get_next_line.h"
+# include "../libft/inc/ft_printf.h"
+# include "../minilibx-linux/mlx.h"
+# include "defines.h"
+# include "structs.h"
+#include <stdio.h>
+
+#endif
