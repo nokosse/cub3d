42bb6e0b0b62b2f49a223b962921a4c8bbcd5c62
nokosse <visouthivong.kevin@gmail.com>
2023-11-13 16:52:21 +0100

    map end

--- src/parsing_map.c
+++ src/parsing_map.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 16:28:55 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/11/13 16:44:41 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/11/13 16:52:08 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int	set_map_start(t_game *game, int i)
 	j = 0;
 	while (game->file_cont[i][j] == ' ')
 		j++;
-	if (game->file_cont[i][j] == '1')
+	if (game->file_cont[i][j] == '1' || game->file_cont[i][j] == '0')
 	{
 		game->map.map_start = i;
 		return (1);
@@ -43,6 +43,7 @@ int	set_map_start(t_game *game, int i)
 	return (0);
 }
 
+// Will check if line (i) is empty but can contain spaces.
 int	is_empty_line(t_game *game, int i)
 {
 	int	j;
@@ -55,22 +56,27 @@ int	is_empty_line(t_game *game, int i)
 	return (0);
 }
 
+// Will check if line (i) is completely empty (only \n at [0])
+int	is_empty_line2(t_game *game, int i)
+{
+	int	j;
+
+	j = 0;
+	if (game->file_cont[i][j] == '\n')
+		return (1);
+	return (0);
+}
+
 int	set_map_end(t_game *game, int *i)
 {
 	int	j;
 
 	j = 0;
-	(*i)++;
 	while (game->file_cont[*i] != NULL)
 	{
-		printf("%s\n", game->file_cont[*i]);
-		while (game->file_cont[*i][j] == ' ')
-			j++;
 		
-		if (game->file_cont[*i][j] == '1')
-			return (0);
-		(*i)++;
 	}
+	game->map.map_end = *i - 1;
 	return (1);
 	(void)game;
 	(void)j;
@@ -85,16 +91,16 @@ int	parse_map(t_game *game)
 	i = game->parse.last_elem + 1;
 	skip_newlines(game, &i);
 	if (set_map_start(game, i) == 0)
-		return (1);
+		return (0);
 	if (set_map_end(game, &i) == 0)
-		return (1);
+		return (0);
 	printf("map start = %d\n", game->map.map_start);
 	printf("map end = %d\n", game->map.map_end);
 	while (game->file_cont[i] != NULL)
 	{
 		i++;
 	}
-	return (0);
+	return (1);
 	(void)j;
 	(void)game;
 }
